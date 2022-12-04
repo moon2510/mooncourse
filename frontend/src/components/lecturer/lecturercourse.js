a11afea9c29e0550da7c98b14f0d39ffe26cf321
moon2510 <nguyetminhthibui@gmail.com>
2022-12-04 21:57:26 +0700

    Display Lecturer Course

---
+++ frontend/src/components/lecturer/lecturercourse.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { AiFillPlusCircle } from "react-icons/ai";
+import Rating from "../home/rating";
+import axios from "axios";
+import "../../styles/lecturerpage/lecturerpage.css"
+
+const LecturerCourseList = () => {
+  const [courses, setCourses] = useState("");
+  const authorId = localStorage.getItem("id");
+
+  const fetchData = async () => {
+    const result = await axios.get(
+      `http://localhost:5000/lecturer/mycourse/${authorId}`
+    );
+    setCourses(result.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (courses === "") {
+    return <div></div>;
+  }
+
+  return (
+    <div className="container">
+      <Link to={"/lecturer/createcourse"} className="wrap-createbutton">
+        <div className="createButton courseCreateButton">
+          <div className="iconCreate">
+            <AiFillPlusCircle size={28} color={"#fff"} />
+          </div>
+          <p> Create</p>
+        </div>
+      </Link>
+      <div className="section">
+        <div className="row">
+          <div className="col-lg-12 col-md-12 article">
+            <div className="coursecontainer row">
+              {courses.map((course) => (
+                <div
+                  className="coursegrid col-lg-4 col-md-6 col-sm-6"
+                  key={course._id}
+                >
+                  <div className="border-course courseCard">
+                    {/* <Link to={`/courses/${course._id}`}>
+                      <div className="courseImage">
+                        <img src={course.image} alt={course.name} />
+                      </div>
+                    </Link> */}
+
+                    <div className="coursetext">
+                      <h3 className="coursename">
+                        <Link to={`/courses/${course._id}`}>{course.name}</Link>
+                      </h3>
+
+                      <Rating value={course.rating} className="rating" />
+                      <p>{course.numberLesson} lessons</p>
+                      <p>Difficulty: {course.level}</p>
+                    </div>
+                    <hr />
+                    <div className="wrapButtonLearnNow">
+                      <div className="price">
+                        {course.price === 0 ? (
+                          <div> Free </div>
+                        ) : (
+                          <div>
+                            <RiMoneyDollarCircleFill
+                              size={25}
+                              color={"#2b8888"}
+                            />{" "}
+                            {course.price}{" "}
+                          </div>
+                        )}
+                      </div>
+                      <Link to={`/courses/${course._id}`}>
+                        <div className="buttonLearnNow">Detail</div>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LecturerCourseList;
